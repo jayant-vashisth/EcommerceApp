0f799451344ac553f0c9261a23b55c8425e0f6c3
jayant-vashisth <jayant.relish@gmail.com>
2023-12-09 18:49:06 +0530

    home screen setup done

--- components/home/Welcome.jsx
+++ components/home/Welcome.jsx
@@ -1,19 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import styles from './welcome.style'
-import { COLORS } from '../../constants'
+import { COLORS, SIZES } from '../../constants'
+import { Feather, Ionicons } from '@expo/vector-icons'
+
+
 
 export default function Welcome() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcomeText(COLORS.black, 0)}>
-        {" "}
-        Find The Most
-      </Text>
-      <Text style={styles.welcomeText(COLORS.black)}>
-        {" "}
-        Luxurious Furniture
-      </Text>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.welcomeText(COLORS.black, 0)}>
+          {" "}
+          Find The Most
+        </Text>
+        <Text style={styles.welcomeText(COLORS.black, 0)}>
+          {" "}
+          Luxurious Furniture
+        </Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Feather
+            name={"search"}
+            size={24}
+            style={styles.searchIcon}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.searchWrapper}>
+          <TextInput
+            value=''
+            style={styles.searchInput}
+            placeholder='What are you looking for'
+            onPressIn={() => { }}
+          />
+        </View>
+
+
+        <View>
+          <TouchableOpacity style={styles.searchBtn}>
+            <Ionicons
+              name={"camera-outline"}
+              style={styles.cameraBtn}
+              size={SIZES.xLarge}
+              color={COLORS.white}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
     </View>
   )
 }
--- components/home/welcome.style.js
+++ components/home/welcome.style.js
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import { COLORS, SIZES } from "../../constants";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,7 +12,42 @@ const styles = StyleSheet.create({
     marginTop: top,
     color: color,
     marginHorizontal: 12
-  })
+  }),
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium,
+    height: 50,
+    marginHorizontal: 12
+  },
+  searchIcon: {
+    marginHorizontal: 10,
+    color: Colors.gray,
+    marginVertical: 10
+  },
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: COLORS.secondary,
+    marginRight: SIZES.small,
+    borderRadius: SIZES.small
+  },
+  searchInput: {
+    fontFamily: 'regular',
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: SIZES.small
+  },
+  searchBtn: {
+    width: 50,
+    height: "100%",
+    borderRadius: SIZES.medium,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primary
+  }
 })
 
 export default styles
